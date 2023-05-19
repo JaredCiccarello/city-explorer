87baac08a0b0a4365fab6213f8c6f693d445a371
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-05-18 21:26:56 -0400

    Completed lab 09

--- src/Movie.js
+++ src/Movie.js
@@ -1,26 +1,28 @@
 import React from "react";
-import Card from 'react-bootstrap/Card'
+import MovieDay from "./MovieDay";
 
 class Movie extends React.Component {
   render() {
+    
     return (
       <>
-        <Card.Title>{this.props.cityName} Movies</Card.Title>
-        <hr></hr>
         {
-          this.props.movieData.map((movie, index) =>
-            <div key={index}>
-              <Card.Text>Title: {movie.title}</Card.Text>
-              <Card.Text>Overview: {movie.overview}</Card.Text>
-              <Card.Text>Average # of Votes: {movie.averageVotes}</Card.Text>
-              <Card.Text>Total # of Votes: {movie.totalVotes}</Card.Text>
-              <Card.Img
-                src={`https://image.tmdb.org/t/p/w500${movie.image_url}`}
-              />
-              <Card.Text>Popularity Level: {movie.popularity}</Card.Text>
-              <Card.Text>Release Date: {movie.releaseDate}</Card.Text>
-              <hr></hr>
-            </div>
+          this.props.movieData.map((movie, idx) =>
+            // <div key={index}>
+            //   <Card.Text>Title: {movie.title}</Card.Text>
+            //   <Card.Text>Overview: {movie.overview}</Card.Text>
+            //   <Card.Text>Average # of Votes: {movie.averageVotes}</Card.Text>
+            //   <Card.Text>Total # of Votes: {movie.totalVotes}</Card.Text>
+            //   <Card.Img
+            //     src={`https://image.tmdb.org/t/p/w500${movie.image_url}`}
+            //   />
+            //   <Card.Text>Popularity Level: {movie.popularity}</Card.Text>
+            //   <Card.Text>Release Date: {movie.releaseDate}</Card.Text>
+            //   <hr></hr>
+            // </div>
+
+            <MovieDay movie = {movie}
+            key={idx}/>
           )
         }
       </>
@@ -31,4 +33,4 @@ class Movie extends React.Component {
 }
 
 
-export default Movie;
+export default Movie
